6891088a6d13fc6cc14a887cd1398e4e64dde947
cdelguercio <cdelguercio@gmail.com>
2012-10-25 17:32:45 -0700

    add Likes to admin panel

--- liquidvirtue/admin.py
+++ liquidvirtue/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from liquidvirtue.models import Video, Channel
+from liquidvirtue.models import Video, Channel, Like
 
 admin.site.register(Video)
 admin.site.register(Channel)
+admin.site.register(Like)
