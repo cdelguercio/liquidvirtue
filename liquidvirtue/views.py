ae7e8652767318a455a3685967f8f7c41028b137
cdelguercio <cdelguercio@gmail.com>
2012-11-07 13:40:38 -0800

    refactor

--- liquidvirtue/views.py
+++ liquidvirtue/views.py
@@ -178,39 +178,7 @@ def trackbox_newest(request, page_number):
 	class_names = []
 	
 	for video in videos:
-		year_delta = now.year - video.upload_date.year
-		month_delta = now.month - video.upload_date.month
-		day_delta = now.day - video.upload_date.day
-		hour_delta = now.hour - video.upload_date.hour
-		minute_delta = now.minute - video.upload_date.minute
-		second_delta = now.second - video.upload_date.second
-	
-		if year_delta > 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' years ago'
-		elif year_delta == 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' year ago'
-		elif month_delta > 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' months ago'
-		elif month_delta == 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' month ago'
-		elif day_delta > 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' days ago'
-		elif day_delta == 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' day ago'
-		elif hour_delta > 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hours ago'
-		elif hour_delta == 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hour ago'
-		elif minute_delta > 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minutes ago'
-		elif minute_delta == 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minute ago'
-		elif second_delta > 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' seconds ago'
-		elif second_delta == 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' second ago'
-		else:
-			upload_date_text = 'Posted now'
+		upload_date_text = get_upload_date_text( video.upload_date, now )
 		upload_date_texts.append(upload_date_text)
 		if Like.objects.filter(user=request.user.id).filter(video=video.id).exists():
 			class_names.append( 'heart' )
@@ -258,39 +226,7 @@ def trackbox_popular(request, page_number):
 	class_names = []
 	
 	for video in videos:
-		year_delta = now.year - video.upload_date.year
-		month_delta = now.month - video.upload_date.month
-		day_delta = now.day - video.upload_date.day
-		hour_delta = now.hour - video.upload_date.hour
-		minute_delta = now.minute - video.upload_date.minute
-		second_delta = now.second - video.upload_date.second
-	
-		if year_delta > 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' years ago'
-		elif year_delta == 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' year ago'
-		elif month_delta > 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' months ago'
-		elif month_delta == 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' month ago'
-		elif day_delta > 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' days ago'
-		elif day_delta == 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' day ago'
-		elif hour_delta > 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hours ago'
-		elif hour_delta == 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hour ago'
-		elif minute_delta > 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minutes ago'
-		elif minute_delta == 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minute ago'
-		elif second_delta > 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' seconds ago'
-		elif second_delta == 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' second ago'
-		else:
-			upload_date_text = 'Posted now'
+		upload_date_text = get_upload_date_text( video.upload_date, now )
 		upload_date_texts.append(upload_date_text)
 		if Like.objects.filter(user=request.user.id).filter(video=video.id).exists():
 			class_names.append( 'heart' )
@@ -314,46 +250,14 @@ def trackbox_my_library(request, page_number):
 	videos = Video.objects.filter(like__user__id__exact=request.user.id).order_by('-upload_time')[(page_number-1)*17:(page_number*17)]
 	#l = Like.objects.filter(user=request.user.id)
 	#videos = l.video_set.order_by('-upload_time')[(page_number-1)*17:(page_number*17)]
-	
+
 	now = datetime.now()
-	
+
 	upload_date_texts = []
 	class_names = []
 	
 	for video in videos:
-		year_delta = now.year - video.upload_date.year
-		month_delta = now.month - video.upload_date.month
-		day_delta = now.day - video.upload_date.day
-		hour_delta = now.hour - video.upload_date.hour
-		minute_delta = now.minute - video.upload_date.minute
-		second_delta = now.second - video.upload_date.second
-	
-		if year_delta > 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' years ago'
-		elif year_delta == 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' year ago'
-		elif month_delta > 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' months ago'
-		elif month_delta == 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' month ago'
-		elif day_delta > 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' days ago'
-		elif day_delta == 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' day ago'
-		elif hour_delta > 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hours ago'
-		elif hour_delta == 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hour ago'
-		elif minute_delta > 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minutes ago'
-		elif minute_delta == 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minute ago'
-		elif second_delta > 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' seconds ago'
-		elif second_delta == 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' second ago'
-		else:
-			upload_date_text = 'Posted now'
+		upload_date_text = get_upload_date_text( video.upload_date, now )
 		upload_date_texts.append(upload_date_text)
 		if Like.objects.filter(user=request.user.id).filter(video=video.id).exists():
 			class_names.append( 'heart' )
@@ -377,44 +281,12 @@ def trackbox_search(request, page_number):
 	videos = Video.objects.filter(title__icontains=search).order_by('-upload_time')[(page_number-1)*17:(page_number*17)]
 
 	now = datetime.now()
-	
+
 	upload_date_texts = []
 	class_names = []
 	
 	for video in videos:
-		year_delta = now.year - video.upload_date.year
-		month_delta = now.month - video.upload_date.month
-		day_delta = now.day - video.upload_date.day
-		hour_delta = now.hour - video.upload_date.hour
-		minute_delta = now.minute - video.upload_date.minute
-		second_delta = now.second - video.upload_date.second
-	
-		if year_delta > 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' years ago'
-		elif year_delta == 1:
-			upload_date_text = 'Posted ' + str(year_delta) + ' year ago'
-		elif month_delta > 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' months ago'
-		elif month_delta == 1:
-			upload_date_text = 'Posted ' + str(month_delta) + ' month ago'
-		elif day_delta > 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' days ago'
-		elif day_delta == 1:
-			upload_date_text = 'Posted ' + str(day_delta) + ' day ago'
-		elif hour_delta > 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hours ago'
-		elif hour_delta == 1:
-			upload_date_text = 'Posted ' + str(hour_delta) + ' hour ago'
-		elif minute_delta > 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minutes ago'
-		elif minute_delta == 1:
-			upload_date_text = 'Posted ' + str(minute_delta) + ' minute ago'
-		elif second_delta > 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' seconds ago'
-		elif second_delta == 1:
-			upload_date_text = 'Posted ' + str(second_delta) + ' second ago'
-		else:
-			upload_date_text = 'Posted now'
+		upload_date_text = get_upload_date_text( video.upload_date, now )
 		upload_date_texts.append(upload_date_text)
 		if Like.objects.filter(user=request.user.id).filter(video=video.id).exists():
 			class_names.append( 'heart' )
@@ -429,46 +301,10 @@ def trackinfo(request, lv_video_id):
 	title = video.title
 	channel_name = video.channel_name
 	watch_page_url = video.watch_page_url
-	
-	upload_date_text = get_upload_date_text( video.upload_date )
-	
-	#upload_date_text = ''
-	
-	#now = datetime.now()
-	
-	#year_delta = now.year - video.upload_date.year
-	#month_delta = now.month - video.upload_date.month
-	#day_delta = now.day - video.upload_date.day
-	#hour_delta = now.hour - video.upload_date.hour
-	#minute_delta = now.minute - video.upload_date.minute
-	#second_delta = now.second - video.upload_date.second
 
-	#if year_delta > 1:
-	#	upload_date_text = 'Posted ' + str(year_delta) + ' years ago'
-	#elif year_delta == 1:
-	#	upload_date_text = 'Posted ' + str(year_delta) + ' year ago'
-	#elif month_delta > 1:
-	#	upload_date_text = 'Posted ' + str(month_delta) + ' months ago'
-	#elif month_delta == 1:
-	#	upload_date_text = 'Posted ' + str(month_delta) + ' month ago'
-	#elif day_delta > 1:
-	#	upload_date_text = 'Posted ' + str(day_delta) + ' days ago'
-	#elif day_delta == 1:
-	#	upload_date_text = 'Posted ' + str(day_delta) + ' day ago'
-	#elif hour_delta > 1:
-	#	upload_date_text = 'Posted ' + str(hour_delta) + ' hours ago'
-	#elif hour_delta == 1:
-	#	upload_date_text = 'Posted ' + str(hour_delta) + ' hour ago'
-	#elif minute_delta > 1:
-	#	upload_date_text = 'Posted ' + str(minute_delta) + ' minutes ago'
-	#elif minute_delta == 1:
-	#	upload_date_text = 'Posted ' + str(minute_delta) + ' minute ago'
-	#elif second_delta > 1:
-	#	upload_date_text = 'Posted ' + str(second_delta) + ' seconds ago'
-	#elif second_delta == 1:
-	#	upload_date_text = 'Posted ' + str(second_delta) + ' second ago'
-	#else:
-	#	upload_date_text = 'Posted now'
+	now = datetime.now()
+
+	upload_date_text = get_upload_date_text( video.upload_date, now )
 
 	likes = Like.objects.filter(user=request.user.id, video_id=lv_video_id)
 	class_name = ''
@@ -481,10 +317,8 @@ def trackinfo(request, lv_video_id):
 		alt_text = 'Add this track to your playlist'
 	return render_to_response('trackinfo.html', {'class_name': class_name, 'alt_text': alt_text, 'title': title, 'channel_name': channel_name, 'upload_date_text': upload_date_text, 'lv_video_id': lv_video_id, 'watch_page_url': watch_page_url})
 
-def get_upload_date_text(upload_date):
+def get_upload_date_text(upload_date, now):
 	upload_date_text = ''
-	
-	now = datetime.now()
 	
 	year_delta = now.year - upload_date.year
 	month_delta = now.month - upload_date.month
